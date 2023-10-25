defbc308362442a812010025bcb88175961b37a4
Jonathan <jongroberg@gmail.com>
2023-10-24 20:26:59 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Data Structures
+- Array
+  
+
+## Array
+
+### Arrays in Nodejs
+
+```javascript
+const a = new ArrayBuffer(6) // => `<00 00 00 00 00 00>`
+const a = new Uint16Array(3) // => `<00 00 00 00 00 00>`
+
+``` 
+### Features
+- fixed size
+- get
+- set
+- delete
+
+# Algorithms
+
+## Search
+
+Goes 1 by 1 throughout the whole array 
+
+**Time Complexity : O(n)**
---
+++ test.ts
@@ -0,0 +1,27 @@
+const a = new Uint16Array(3);
+const b = new Uint8Array(a.buffer);
+
+console.log(a);
+console.log(b);
+
+
+
+//Linear Search returing the index of the target element
+function linearSearch(array: number[], targetElement: number):number{
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === targetElement){
+            return i;
+        }
+    }
+    throw new Error("Element not found");
+}
+
+console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 5));
+
+//Binary Search
+
+function binarySearch(array, targetElement){
+
+}
+
+//
