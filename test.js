aa91fbce75729d97b79eb0db59dde0481122e94a
Jonathan <jongroberg@gmail.com>
2023-10-30 09:47:47 -0400

    commented out unused code

--- test.js
+++ test.js
@@ -1,27 +1,39 @@
-const a = new Uint16Array(3);
-const b = new Uint8Array(a.buffer);
+// const a = new Uint16Array(3);
+// const b = new Uint8Array(a.buffer);
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
+// //Performance Measure
+// function timing(func, ...args) {
+//   let time = 0;
+//   let trials = 1000;
+//   for (let i = 0; i < trials; i++) {
+//     const t0 = performance.now();
+//     func(args[0], args[1]);
+//     const t1 = performance.now();
+//     time += t1 - t0;
+//   }
 
+//   console.log(`took ${(time / trials).toFixed(4)} milliseconds`);
+// }
 
 //Linear Search returing the index of the target element
-function linearSearch(array, targetElement){
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === targetElement){
-            return i;
-        }
+function linearSearch(array, targetElement) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === targetElement) {
+      return i;
     }
-    throw new Error("Element not found");
+  }
+  throw new Error("Element not found");
 }
 
-console.log(linearSearch([1,2,3,4,5,6,7,8,9,10], 5));
+// console.log(linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
 
 //Binary Search
 
-function binarySearch(array, targetElement){
-
+function binarySearch(array, targetElement) {
+    
 }
 
-//
+// timing(linearSearch, [1, 2, 4, 5, 6, 7, 9, 8, 66, 434], 66);
