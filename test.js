520412c95ca0c61377991e715180b837ada452a6
Jonathan Groberg <jongroberg@gmail.com>
2024-03-02 00:20:13 -0500

    Refactor stringPatternSearch function to improve readability and efficiency

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
 function stringPatternSearch(text, pattern) {
-	let patternArr = pattern.split("");
 	let count = 0;
-	let matchingIndex = 0;
+	let patternIndex = 0;
 
 	for (let i = 0; i < text.length; i++) {
-		if ((text[i] == pattern[matchingIndex])) {
-      if (matchingIndex == pattern.length-1) count++
-      else matchingIndex++;
-		}else{
-      matchingIndex=0;
-    }
+		if (text[i] === pattern[patternIndex]) { //checks if
+			if (patternIndex === pattern.length - 1) count++; //check if the index as at the end of the pattern
+			else patternIndex++; // else increase the index
+		} else { // reset the count of the variable is not
+			patternIndex = 0;
+		}
 	}
-  return count;
+	return count;
 }
 
-console.log(stringPatternSearch("testing testing 123","testing"));
+console.log(stringPatternSearch("testing testing 123 test testing", "testing"));
